0c80f7ef0d06d3ff2ca8613ea8a7091242adceb1
changshuai <chshuai.mail@gmail.com>
2015-01-25 18:02:04 +0800

    1.fix a memory leaks bug 2.add .proj for android and mac-ios

---
+++ proj.android/jni/Android.mk
@@ -0,0 +1,50 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/external)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/cocos)
+
+LOCAL_MODULE := cocos2dcpp_shared
+
+LOCAL_MODULE_FILENAME := libcocos2dcpp
+
+define all-cpp-files-under
+$(patsubst ./%,%, $(shell find $(1) -name "*.c" -o -name "*.cpp"))
+endef  
+  
+define all-subdir-cpp-files  
+$(call all-cpp-files-under, $(LOCAL_PATH)/../../Classes/Gif)  
+endef
+GIF_SRC_FILES := $(all-subdir-cpp-files) \
+
+LOCAL_SRC_FILES := hellocpp/main.cpp \
+                   ../../Classes/AppDelegate.cpp \
+                   ../../Classes/HelloWorldScene.cpp \
+                   $(GIF_SRC_FILES:$(LOCAL_PATH)/%=%)
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+
+LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
+
+# LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
+# LOCAL_WHOLE_STATIC_LIBRARIES += cocosbuilder_static
+# LOCAL_WHOLE_STATIC_LIBRARIES += spine_static
+# LOCAL_WHOLE_STATIC_LIBRARIES += cocostudio_static
+# LOCAL_WHOLE_STATIC_LIBRARIES += cocos_network_static
+# LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
+
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,.)
+$(call import-module,audio/android)
+
+# $(call import-module,Box2D)
+# $(call import-module,editor-support/cocosbuilder)
+# $(call import-module,editor-support/spine)
+# $(call import-module,editor-support/cocostudio)
+# $(call import-module,network)
+# $(call import-module,extensions)
